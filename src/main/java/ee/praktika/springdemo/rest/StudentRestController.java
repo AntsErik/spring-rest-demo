0e9da183f87771233b0ed38e32c9a1fcfa222e34
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-24 15:55:35 +0300

    Adding exceptionhandler for any type of exception (text conversion to number etc).

--- src/main/java/ee/praktika/springdemo/rest/StudentRestController.java
+++ src/main/java/ee/praktika/springdemo/rest/StudentRestController.java
@@ -62,4 +62,19 @@ public class StudentRestController {
         // return the response entity
         return new ResponseEntity<>( error, HttpStatus.NOT_FOUND );
     }
+
+    //Add an additional exception handler for any exception
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException( Exception exc ){
+
+        //create the Student Error responst
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus( HttpStatus.BAD_REQUEST.value() );
+        error.setMessage( exc.getMessage() );
+        error.setTimeStamp( System.currentTimeMillis() );
+
+        // return the response entity
+        return new ResponseEntity<>( error, HttpStatus.BAD_REQUEST );
+    }
 }
